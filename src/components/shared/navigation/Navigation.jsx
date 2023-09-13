b0914dfbccce2d644385ee9e46685a4ea27b068f
reesdela <reesdela@gmail.com>
2023-09-12 23:11:56 -0500

    add list seperator as list item...for now

--- src/components/shared/navigation/Navigation.jsx
+++ src/components/shared/navigation/Navigation.jsx
@@ -13,23 +13,44 @@ const Navigation = () => {
                     <li className="nav-list-item">
                         <a href="">HOME</a>
                     </li>
+                    <li className="nav-list-seperator">
+                    |
+                    </li>
                     <li className="nav-list-item">
                         <a href="">CERAMIC FISH ART</a>
+                    </li>
+                    <li className="nav-list-seperator">
+                    |
                     </li>
                     <li className="nav-list-item">
                         <a href="">CUSTOM ORDER</a>
                     </li>
+                    <li className="nav-list-seperator">
+                    |
+                    </li>
                     <li className="nav-list-item">
                         <a href="">SALE</a>
+                    </li>
+                    <li className="nav-list-seperator">
+                    |
                     </li>
                     <li className="nav-list-item">
                         <a href="">SHIPPING</a>
                     </li>
+                    <li className="nav-list-seperator">
+                    |
+                    </li>
                     <li className="nav-list-item">
                         <a href="">ABOUT US</a>
                     </li>
+                    <li className="nav-list-seperator">
+                    |
+                    </li>
                     <li className="nav-list-item">
                         <a href="">FAQ</a>
+                    </li>
+                    <li className="nav-list-seperator">
+                    |
                     </li>
                     <li className="nav-list-item">
                         <a href="">CONTACT US</a>
